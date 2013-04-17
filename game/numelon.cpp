15a6d2c3dce051c2985b62c71b3f8feb10a6c268
sinsunsik420 <sinsunsik420@gmail.com>
2013-04-17 15:03:52 +0900

    some script is deleted, and added.

--- game/numelon.cpp
+++ game/numelon.cpp
@@ -1,36 +1,61 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
 using namespace std;
 
-char generate_ans(){
+int generate_ans(){
   srand((unsigned int)time(NULL));
   return ( (rand()+1) % 10 );
 }
 
 int main(){
-  char x;
-  char y;
-  char z;
+  int x;
+  int y;
+  int z;
   
-  char ans_x[5];
-  char ans_y[5];
-  char ans_z[5];
-  
-  sprintf(ans_x,"%d",generate_ans());
+  int ans[3];
+
+  int i = 0;
+  char* p;
+
+  cout << "*******Please wait*******" << endl;
+  ans[0] = generate_ans();
+  sleep(1);
+  ans[1] = generate_ans();
+  sleep(1);
+  ans[2] = generate_ans();
   
   cout << "game start!" << endl;
-  cout << "一番目の数字を入力して下さい。＞" << endl;
-  cin >> x;
-  cout << "二番目の数字を入力して下さい。＞" << endl;
-  cin >> y;
-  cout << "三番目の数字を入力して下さい。＞" << endl;
-  cin >> z;
+  
+  while(i<=10){
+    int flag = 0;
 
-  cout << ans_x << endl;
+    cout << "一番目の数字を入力して下さい。＞" << endl;
+    cin >> x;    
+    if(ans[0] == x) flag++;
+
+    cout << "二番目の数字を入力して下さい。＞" << endl;
+    cin >> y;
+    if(ans[1] == y) flag++;
+
+    cout << "三番目の数字を入力して下さい。＞" << endl;
+    cin >> z;
+    if(ans[2] == z) flag++;
+
+    if(flag == 3){
+      cout << "game set!\n" << "You Win!\n" << endl;
+      break;
+    }else{
+      cout << "This turn is " << flag << " Hits!\nLeast "<< 10-i << "turns.\nnext turn!\n" << endl;
+    }
+    i++;
+  }
+
+  if(i>10) cout << "game set!\n" << "You Lose wwwwwwwwwwwwww!!!\n" << endl;
+  cout << "The answer is " << ans[0] << ans[1] << ans[2] << ".\nSee you next game.\n";
 
   return 0;
 }
